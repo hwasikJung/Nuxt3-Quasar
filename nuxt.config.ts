d78b5456bf63900b5e8544d260f7f677f7045868
hwasikJung <hs.jung@sqisoft.com>
2024-01-22 11:56:05 +0900

    - pinia add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: [
-        'nuxt-quasar-ui'
-      ],
-    quasar: {
-        plugins: [
-            'Notify'
-          ],
-        extras: {
-            fontIcons: ['material-icons']
-        }
+  modules: ['nuxt-quasar-ui', '@pinia/nuxt'],
+  quasar: {
+    plugins: ['Notify'],
+    extras: {
+      fontIcons: ['material-icons']
     }
+  }
+  // imports: {
+  //   dirs: ['./stores']
+  // },
+  // pinia: {
+  //   autoImports: ['defineStore', 'acceptHMRUpdate']
+  // }
 })
---
+++ pages/counter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <input type="button" value="increment" @click="store.increment()" />
+    {{ store.count }}
+    {{ store.doubleCount }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useCounterStore } from '~/stores/counter'
+
+const store = useCounterStore()
+</script>
+
+<style scoped></style>
